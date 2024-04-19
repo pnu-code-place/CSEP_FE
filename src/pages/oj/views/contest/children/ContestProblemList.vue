bbdffe3b0c6df71b1ec18b0a00577f96bc8cd18e
llddang <bodyness@naver.com>
2024-04-19 13:11:03 +0900

    design: 대회 문제 table 생성

--- src/pages/oj/views/contest/children/ContestProblemList.vue
+++ src/pages/oj/views/contest/children/ContestProblemList.vue
@@ -4,6 +4,38 @@
     <div class="problemTitle">
       <p>{{$t('m.Problems_List')}}</p>
     </div>
+    <div class="problemContent">
+      <table v-if="problems.length !== 0" style="text-align: center;">
+        <thead>
+          <th>{{ $t('m.Th_Problem_Id') }}</th>
+          <th>{{ $t('m.Th_Problem_Title') }}</th>
+          <th>{{ $t('m.Th_Problem_Difficulty') }}</th>
+          <th>{{ $t('m.Th_Problem_Total_Score') }}</th>
+          <th>{{ $t('m.Th_Problem_AC_Rate') }}</th>
+        </thead>
+        <tbody>
+          <tr v-for="problem in problems" @click="goContestProblem(problem._id)">
+            <td>{{problem._id}}</td>
+            <td style="display: flex; flex-direction: column; height: fit-content;">
+              <span>{{problem.title}}</span>
+              <div style="display: flex;">
+                <FieldCategoryBox :boxType="true" :value="FIELD_MAP[problem.field].value"
+                                  :boxColor="FIELD_MAP[problem.field].boxColor" />
+                <template v-for="(category, idx) in problem.tags">
+                  <FieldCategoryBox :boxType="false" :value="'#' + category" :boxColor="'#ffffff'"/>
+                </template>
+              </div>
+            </td>
+            <td>{{DIFFICULTY_MAP[problem.difficulty].value}}</td>
+            <td>{{problem.total_score}}</td>
+            <td>{{getACRate(problem.accepted_number, problem.submission_number)}}</td>
+          </tr>
+        </tbody>
+      </table>
+      <div v-else style="text-align: center; font-size: 16px;">
+        {{$t('m.No_Problems')}}
+      </div>
+    </div>
   </div>
   <div>
     <Panel>
@@ -26,9 +58,12 @@
 <script>
   import {mapState, mapGetters} from 'vuex'
   import {ProblemMixin} from '@oj/components/mixins'
+  import {DIFFICULTY_MAP, FIELD_MAP} from "../../../../../utils/constants";
+  import FieldCategoryBox from "../../../components/FieldCategoryBox.vue";
 
   export default {
     name: 'ContestProblemList',
+    components: {FieldCategoryBox},
     mixins: [ProblemMixin],
     data () {
       return {
@@ -82,12 +117,12 @@
           }
         })
       },
-      goContestProblem (row) {
+      goContestProblem (id) {
         this.$router.push({
           name: 'contest-problem-details',
           params: {
             contestID: this.$route.params.contestID,
-            problemID: row._id
+            problemID: id
           }
         })
       }
@@ -96,7 +131,13 @@
       ...mapState({
         problems: state => state.contest.contestProblems
       }),
-      ...mapGetters(['isAuthenticated', 'contestRuleType', 'OIContestRealTimePermission'])
+      ...mapGetters(['isAuthenticated', 'contestRuleType', 'OIContestRealTimePermission']),
+      DIFFICULTY_MAP() {
+        return DIFFICULTY_MAP
+      },
+      FIELD_MAP() {
+        return FIELD_MAP
+      },
     }
   }
 </script>
