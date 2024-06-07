130be161e3e7847f7cd27806996e9c8a91b9e741
llddang <bodyness@naver.com>
2024-06-07 15:29:37 +0900

    feat: Contest History 페이지 생성 & 라우터 연결

---
+++ src/pages/oj/views/contest/ContestHistory.vue
@@ -0,0 +1,15 @@
+<template>
+  <main>Contest History</main>
+</template>
+
+<script>
+export default {
+  name: "contest-history-list",
+};
+</script>
+
+<style lang="less" scoped>
+main {
+  width: 1200px;
+}
+</style>
